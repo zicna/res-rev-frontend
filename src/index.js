52c6ccd081173eb49ba6a17d801abe0590da23e9
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-12-09 15:28:29 -0600

    add bootswatch

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import App from './App';
 import { Provider } from 'react-redux'
 import store from './redux/store'
 
+import 'bootswatch/dist/materia/bootstrap.min.css'
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <App className="list-group"/>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
