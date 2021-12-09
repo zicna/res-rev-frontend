9c4d57b2aca5ae4a00f1b5f648acbbfcbb3e2b41
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-12-09 13:07:20 -0600

    change App.js to render Layout page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import RestaurantContainer from "./components/RestaurantContainer";
 import MainHeader from './components/MainHeader'
 
 import { BrowserRouter as Router} from 'react-router-dom'
+import Layout from "./pages/Layout";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <MainHeader />
-        <RestaurantContainer />
+        <Layout />
       </Router>
     </div>
   );
