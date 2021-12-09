4f088b05843f6f96b9f9822060df2ff7318b2b49
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-12-09 15:50:08 -0600

    add style to nav-bar

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -4,34 +4,38 @@ import { NavLink } from 'react-router-dom'
 export default function MainHeader() {
     return (
         <header>
-            <nav>
-                <ul>
-                    <li>
-                        <NavLink 
-                            to="/"
-                            // activeClassName={}
-                        >
-                        Home
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink 
-                            to="/about"
-                            // activeClassName={}
-                        >
-                        About
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink 
-                            to="/restaurants"
-                            // activeClassName={}
-                        >
-                        Restaurants
-                        </NavLink>
-                    </li>
-
-                </ul>
+            <nav className='navbar navbar-expand-lg navbar-dark bg-primary'>
+                <div className='collapse navbar-collapse'>
+                    <ul className='navbar-nav me-auto'>
+                        <li className='nav-item'>
+                            <NavLink
+                                to='/'
+                                // activeClassName={active}
+                                className='nav-link'
+                            >
+                            Home
+                            </NavLink>
+                        </li>
+                        <li className='nav-item'>
+                            <NavLink
+                                to='/about'
+                                // activeClassName={{active}}
+                                className='nav-link'
+                            >
+                            About
+                            </NavLink>
+                        </li>
+                        <li className='nav-item'>
+                            <NavLink
+                                to='/restaurants'
+                                // activeClassName={{active}}
+                                className='nav-link'
+                            >
+                            Restaurants
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
             </nav>
         </header>
     )
