b7a714a5cdae1a7c0c25727b61ac65a6e59b8851
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-12-09 13:20:47 -0600

    restaurantCOntainer can fetch data from backend

--- src/components/RestaurantContainer.js
+++ src/components/RestaurantContainer.js
@@ -1,13 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-class RestaurantContainer extends Component{
-    render(){
-        return(
-            <div>
-                Restaurant Container
-            </div>
-        )
-    }
+class RestaurantContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      restaurants: [],
+    };
+  }
+
+  getRestaurants = () => {
+      console.log('helllo')
+    fetch('http://localhost:3001/restaurants')
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
+  render() {
+    return (
+      <div>
+        <button onClick={this.getRestaurants}>Show all Restaurants</button>
+      </div>
+    );
+  }
 }
 
-export default RestaurantContainer
+
+
+export default RestaurantContainer;
