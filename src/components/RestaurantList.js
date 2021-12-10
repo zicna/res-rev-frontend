30cd20395d6bb92c12493c3c882e185d98ec37f9
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-12-10 13:39:23 -0600

    cleanup

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,53 +1,46 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import RestaurantCard from './RestaurantCard'
-import RestaurantForm from './RestaurantForm'
-
-
+import RestaurantCard from "./RestaurantCard";
+import RestaurantForm from "./RestaurantForm";
 
 export default class RestaurantList extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            newRestaurant: false,
-            toggleForm: () => this.toggleForm()
-        }
-    }
-    toggleForm = () => {
-        this.setState(prevState => ({
-            newRestaurant: !prevState
-        }
-        ))
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      newRestaurant: false,
+      toggleForm: () => this.toggleForm(),
+    };
+  }
+  toggleForm = () => {
+    this.setState((prevState) => ({
+      newRestaurant: !prevState,
+    }));
+  };
 
-    handleNewResClick = () => {
-        console.log('hello')
-        this.setState({
-            newRestaurant: true
-        })
-    }
-    render() {
-        return (
+  handleNewResClick = () => {
+    this.setState({
+      newRestaurant: true,
+    });
+  };
+  render() {
+    return (
+      <div>
+        {this.state.newRestaurant ? (
+          <RestaurantForm toggleForm={this.state.toggleForm} />
+        ) : (
+          <button onClick={this.handleNewResClick}>Add new restaurant</button>
+        )}
+        {this.props.restaurants.map((res) => {
+          return (
             <div>
-                {this.state.newRestaurant ? 
-                    < RestaurantForm toggleForm={this.state.toggleForm}/> : 
-                    <button 
-                        onClick={this.handleNewResClick}>
-                        Add new restaurant
-                    </button>}
-                {this.props.restaurants.map(res => {
-                    return (
-                    <div>
-                        <p key={res.id}>{res.name}</p>
-                        <button>Edit</button>
-                        <button>Delete</button>
-                        <button>Show</button>
-                    </div>
-                    )})}
-
-
-                
+              <p key={res.id}>{res.name}</p>
+              <button>Edit</button>
+              <button>Delete</button>
+              <button>Show</button>
             </div>
-        )
-    }
+          );
+        })}
+      </div>
+    );
+  }
 }
