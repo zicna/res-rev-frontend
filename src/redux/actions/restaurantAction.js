7b4366bbf9cbd190e223374ff9932e58ba239128
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-12-10 15:05:20 -0600

    add action for adding new restaurant

--- src/redux/actions/restaurantAction.js
+++ src/redux/actions/restaurantAction.js
@@ -1,6 +1,7 @@
 // * thunk gives us acces to dispatch within action creator
 
 const GET_RESTAURANTS = "GET_RESTAURANTS";
+const ADD_RESTAURANT = "ADD_RESTAURANT";
 
 const resURL = "http://localhost:3001/restaurants";
 
@@ -9,7 +10,23 @@ export const getRestaurants = () => {
     fetch(resURL)
       .then((response) => response.json())
       .then((data) => {
-          dispatch({type: GET_RESTAURANTS, payload: data})
-        });
+        dispatch({ type: GET_RESTAURANTS, payload: data });
+      });
+  };
+};
+
+export const addRestaurant = (restaurantObject) => {
+  return (dispatch) => {
+    fetch(resURL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(restaurantObject),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        dispatch({ type: ADD_RESTAURANT, payload: data });
+      });
   };
 };
