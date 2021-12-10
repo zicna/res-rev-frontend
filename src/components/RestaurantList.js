8fc01f247d81a4ff286e1c539c808d00b7a650c4
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-12-10 13:18:44 -0600

    add callback to local state to toggle form rendering

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -9,12 +9,41 @@ export default class RestaurantList extends Component {
     constructor(props){
         super(props)
         this.state = {
-            newRestaurant: false
+            newRestaurant: false,
+            toggleForm: () => this.toggleForm()
         }
+    }
+    toggleForm = () => {
+        this.setState(prevState => ({
+            newRestaurant: !prevState
+        }
+        ))
+    }
+
+    handleNewResClick = () => {
+        console.log('hello')
+        this.setState({
+            newRestaurant: true
+        })
     }
     render() {
         return (
             <div>
+                {this.state.newRestaurant ? 
+                    < RestaurantForm toggleForm={this.state.toggleForm}/> : 
+                    <button 
+                        onClick={this.handleNewResClick}>
+                        Add new restaurant
+                    </button>}
+                {this.props.restaurants.map(res => {
+                    return (
+                    <div>
+                        <p key={res.id}>{res.name}</p>
+                        <button>Edit</button>
+                        <button>Delete</button>
+                        <button>Show</button>
+                    </div>
+                    )})}
 
 
                 
