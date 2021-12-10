263aa2b877bec409f045cee1ab68e8cee2cacd57
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-12-10 13:52:04 -0600

    format ad style RestaurantForm component

--- src/components/RestaurantForm.js
+++ src/components/RestaurantForm.js
@@ -1,76 +1,101 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class RestaurantForm extends Component {
-    constructor(props){
-        super(props)
-            this.state={
-                name: '',
-                location: '',
-                res_type: '',
-                image: '',
-                description: ''
-            }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      location: "",
+      res_type: "",
+      image: "",
+      description: "",
+    };
+  }
 
-    handleChange = (event) => {
-        this.setState({
-            [event.target.name]: event.target.value
-        })
-    }
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
 
-    handleSubmit = (event) => {
-        event.preventDefault();
+  handleSubmit = (event) => {
+    event.preventDefault();
 
-        // * reset local state after submiting form 
-        this.setState({
-            name: '',
-            location: '',
-            res_type: '',
-            image: '',
-            description: ''
-        })
-        this.props.toggleForm()
-    }
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <input 
-                    type="text" 
-                    name='name' 
-                    value={this.state.name} 
-                    onChange={this.handleChange} 
+    // * reset local state after submiting form
+    this.setState({
+      name: "",
+      location: "",
+      res_type: "",
+      image: "",
+      description: "",
+    });
+    this.props.toggleForm();
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <fieldset>
+          <legend>New Restaurant Legend</legend>
+          <div className="form-group row">
+              <label className='col-sm-2 col-form-label'>Name</label>
+              <div className="col-sm-10">
+                <input
+                  type="text"
+                  name="name"
+                  value={this.state.name}
+                  onChange={this.handleChange}
                 />
-                <input 
-                    type="text" 
-                    name='location' 
-                    value={this.state.location} 
-                    onChange={this.handleChange} 
+              </div>
+          </div>
+        <div className="form-group row">
+            <label className='col-sm-2 col-form-label'>Location</label>
+              <div className="col-sm-10">
+                <input
+                  type="text"
+                  name="location"
+                  value={this.state.location}
+                  onChange={this.handleChange}
                 />
-                <input 
-                    type="text" 
-                    name='res_type' 
-                    value={this.state.res_type} 
-                    onChange={this.handleChange} 
+              </div>
+        </div>
+        <div className="form-group row">
+            <label className='col-sm-2 col-form-label'>Restaurant type</label>
+              <div className="col-sm-10">
+                <input
+                  type="text"
+                  name="res_type"
+                  value={this.state.res_type}
+                  onChange={this.handleChange}
                 />
-                <input 
-                    type="text" 
-                    name='image' 
-                    value={this.state.image} 
-                    onChange={this.handleChange} 
+              </div>
+        </div>
+        <div className="form-group row">
+            <label className='col-sm-2 col-form-label'>Image</label>
+              <div className="col-sm-10">
+                <input
+                  type="text"
+                  name="image"
+                  value={this.state.image}
+                  onChange={this.handleChange}
                 />
-                <input 
-                    type="text" 
-                    name='description' 
-                    value={this.state.description} 
-                    onChange={this.handleChange} 
+              </div>
+        </div>
+        <div className="form-group row">
+            <label className='col-sm-2 col-form-label'>Description</label>
+              <div className="col-sm-10">
+                <input
+                  type="text"
+                  name="description"
+                  value={this.state.description}
+                  onChange={this.handleChange}
                 />
-                <input 
-                    type="submit" 
-                    value='Create New Restaurant'
-                />
-
-                
-            </form>
-        )
-    }
+              </div>
+        </div>
+          <div className="col-sm-10">
+            <input type="submit" value="Create New Restaurant" />
+          </div>
+        </fieldset>
+      </form>
+    );
+  }
 }
