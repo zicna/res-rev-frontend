da476402a22b34d1a53fb2b06b9752d1c8287282
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-12-11 17:49:03 -0600

    pass restorants to rendering components

--- src/pages/RoutesLayout.js
+++ src/pages/RoutesLayout.js
@@ -1,26 +1,26 @@
-// ! here we defined basic routes
-// ! we are using render prop to pass a callback so we can have access to history, location, match,... in component that are beeing rendered
+// * here we defined basic routes
+// * we are using render prop to pass a callback so we can have access to history, location, match,... in component that are beeing rendered
 import { Route, Switch } from "react-router-dom";
 
 import Home from "./Home";
 import About from "./About";
 import Contact from './Contact'
-import Restaurants from '../components/Restaurant/Restaurants'
-import RestaurantCard from '../components/Restaurant/RestaurantCard'
-import RestaurantForm from '../components/Restaurant/RestaurantForm'
+import Restaurants from '../components/restaurant/Restaurants'
+import RestaurantCard from '../components/restaurant/RestaurantCard'
+import RestaurantForm from '../components/restaurant/RestaurantForm'
 
-const Layout = () => {
+const RouteLayout = ({restaurants}) => {
   return (
     <Switch>
         <Route exact path='/' component={routerProps => < Home {...routerProps} />} />
         <Route exact path='/about' component={routerProps => < About {...routerProps}/>} />
         <Route exact path='/contact' component={routerProps => < Contact {...routerProps}/>} />
-        <Route exact path='/restaurants' component={routerProps => < Restaurants {...routerProps}/>} />
-        <Route exact path='/restaurants/:id' component={routerProps => < RestaurantCard {...routerProps}/>} />
-        <Route exact path='/restaurants/:id/edit' component={routerProps => < RestaurantForm {...routerProps}/>} />
-        <Route exact path='/restaurants/new' component={routerProps => < RestaurantForm {...routerProps}/>} />
+        <Route exact path='/restaurants' component={routerProps => < Restaurants {...routerProps} restaurants={restaurants} />} />
+        <Route exact path='/restaurants/:id' component={routerProps => < RestaurantCard {...routerProps} restaurants={restaurants} />} />
+        <Route exact path='/restaurants/:id/edit' component={routerProps => < RestaurantForm {...routerProps} restaurants={restaurants} />} />
+        <Route exact path='/restaurants/new' component={routerProps => < RestaurantForm {...routerProps} restaurants={restaurants} />} />
     </Switch>
   );
 };
 
-export default Layout;
+export default RouteLayout;
