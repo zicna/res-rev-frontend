f6dc0bc44fd381ab9ebcfc7d00d4bd49632508b1
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-12-11 17:44:32 -0600

    Connect to Redux

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,23 +1,32 @@
-// ! Connect with Redux and pass props to MainLayout
+// * Connect with Redux and pass props to MainLayout
 
 import React, { Component } from 'react'
-import RoutesLayout from "./pages/RoutesLayout";
-
+import RoutesLayout from "../pages/RoutesLayout";
 import { connect } from 'react-redux'
-
-
-
+import {getRestaurants} from '../redux/actions/restaurantAction'
 class MainContainer extends Component {
+
+    componentDidMount(){
+        this.props.dispatchSetRestaurants()
+    }
     render() {
         return (
             <div>
-                <RoutesLayout />
+                <RoutesLayout restaurants={this.props.restaurants}/>
             </div>
         )
     }
 }
 
+const mapStateToProps = (state) => {
+    return {
+      ...state.restaurants
+    }
+  }
+  const mapDispatchToProps = (dispatch) => {
+    return {
+      dispatchSetRestaurants: () => dispatch(getRestaurants())
+    }
+  }
 
-
-
-export default connect()(MainContainer)
+export default connect(mapStateToProps, mapDispatchToProps)(MainContainer)
