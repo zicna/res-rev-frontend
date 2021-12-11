4278f3fe529630c81b1712464ad9a6f561fabc2d
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-12-11 17:55:35 -0600

    add contact link to NavBar and clenup component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,42 +1,35 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function MainHeader() {
-    return (
-        <header>
-            <nav className='navbar navbar-expand-lg navbar-dark bg-primary'>
-                <div className='collapse navbar-collapse'>
-                    <ul className='navbar-nav me-auto'>
-                        <li className='nav-item'>
-                            <NavLink
-                                to='/'
-                                // activeClassName={active}
-                                className='nav-link'
-                            >
-                            Home
-                            </NavLink>
-                        </li>
-                        <li className='nav-item'>
-                            <NavLink
-                                to='/about'
-                                // activeClassName={{active}}
-                                className='nav-link'
-                            >
-                            About
-                            </NavLink>
-                        </li>
-                        <li className='nav-item'>
-                            <NavLink
-                                to='/restaurants'
-                                // activeClassName={{active}}
-                                className='nav-link'
-                            >
-                            Restaurants
-                            </NavLink>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
-    )
+  return (
+    <header>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav me-auto">
+            <li className="nav-item">
+              <NavLink to="/" className="nav-link">
+                HOME
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/about" className="nav-link">
+                ABOUT
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/contact" className="nav-link">
+                CONTACT
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/restaurants" className="nav-link">
+                Restaurants
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
 }
