4bd7a283f14010d04a0800181970be37e0b4e711
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-12-11 17:48:19 -0600

    clenup MainContainer

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,32 +1,31 @@
 // * Connect with Redux and pass props to MainLayout
 
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import RoutesLayout from "../pages/RoutesLayout";
-import { connect } from 'react-redux'
-import {getRestaurants} from '../redux/actions/restaurantAction'
+import { connect } from "react-redux";
+import { getRestaurants } from "../redux/actions/restaurantAction";
 class MainContainer extends Component {
-
-    componentDidMount(){
-        this.props.dispatchSetRestaurants()
-    }
-    render() {
-        return (
-            <div>
-                <RoutesLayout restaurants={this.props.restaurants}/>
-            </div>
-        )
-    }
+  componentDidMount() {
+    this.props.dispatchSetRestaurants();
+  }
+  render() {
+    return (
+      <div>
+        <RoutesLayout restaurants={this.props.restaurants} />
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
-    return {
-      ...state.restaurants
-    }
-  }
-  const mapDispatchToProps = (dispatch) => {
-    return {
-      dispatchSetRestaurants: () => dispatch(getRestaurants())
-    }
-  }
+  return {
+    ...state.restaurants,
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    dispatchSetRestaurants: () => dispatch(getRestaurants()),
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(MainContainer)
+export default connect(mapStateToProps, mapDispatchToProps)(MainContainer);
