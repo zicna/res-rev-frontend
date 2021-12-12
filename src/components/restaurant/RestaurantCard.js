8ef85bbb573c02daef17dd24269bb87d7a8e36d7
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-12-11 23:04:49 -0600

    add delete functionality to RestaurantCard

--- src/components/restaurant/RestaurantCard.js
+++ src/components/restaurant/RestaurantCard.js
@@ -1,7 +1,20 @@
 import React, { Component } from "react";
 import ImageContainer from "../ImageContainer";
+import { connect } from 'react-redux'
+import { deleteRestaurant } from "../../redux/actions/restaurantAction";
 
-export default class RestaurantCard extends Component {
+class RestaurantCard extends Component {
+  
+  handleEditClick = () => {
+    console.log("Edit was clicked")
+  }
+  handleDeleteClick =() =>{
+    const { match, restaurants } = this.props;
+    const resObj = restaurants[match.params.id - 1]
+    this.props.dispatchDeleteRestaurant(resObj.id)
+    this.props.history.push("/restaurants")
+
+  }
   renderRestaurant = () => {
     const { match, restaurants } = this.props;
     const resObj = restaurants[match.params.id - 1];
@@ -18,14 +31,23 @@ export default class RestaurantCard extends Component {
         </div>
         <div className="card-body">Here we will list reviews</div>
         <div className="card-body">
-          <a href="">Link for Edit Restaurant</a>
-
-          <a href="">Link for delete Restaurant</a>
+          <button onClick={this.handleEditClick}>Edit</button>
+          <button onClick={this.handleDeleteClick}>Delete</button>
         </div>
       </div>
     );
   };
+
+
   render() {
     return <div>{this.renderRestaurant()}</div>;
   }
 }
+
+const mapDispatchToProps = (dispatch) => {
+  return{
+    dispatchDeleteRestaurant: (id) => dispatch(deleteRestaurant(id))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(RestaurantCard)
