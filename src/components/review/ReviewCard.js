d5ab392b178f93bd16d4abfbc54ad8547dc12c05
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-12-12 00:28:33 -0600

    add review folder with basic files for reviews

---
+++ src/components/review/ReviewCard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ReviewCard extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
---
+++ src/components/review/Reviews.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Reviews extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
