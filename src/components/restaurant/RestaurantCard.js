4aff044017014901da47aa177f167b8532b200c4
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-12-11 20:49:16 -0600

    RestaurantCard renders restaurant from props based of params in url

--- src/components/restaurant/RestaurantCard.js
+++ src/components/restaurant/RestaurantCard.js
@@ -1,32 +1,35 @@
-import React, { Component } from 'react'
-import ImageContainer from '../ImageContainer'
+import React, { Component } from "react";
+import ImageContainer from "../ImageContainer";
 
 export default class RestaurantCard extends Component {
-    render() {
-        return (
-            <div> Hello From Restaurants Card</div>
-        )
-    }
+  renderRestaurant = () => {
+    const { match, restaurants } = this.props;
+    const resObj = restaurants[match.params.id - 1];
+
+    // const {description, name, rating, image, res_type, location, reviews} = restaurants[match.params.id - 1]
+    // console.log(resObj.name)
+    // debugger
+    // const {description, name, rating, image, res_type, location, reviews} = resObj
+    return (
+      <div className="card mb-3" style={{ width: "75%" }}>
+        <h3 className="card-header">{resObj.name}</h3>
+        <div className="card-body">
+          <h6 className="card-subtitle text-muted">{resObj.res_type}</h6>
+        </div>
+        <ImageContainer image={resObj.image} />
+        <div className="card-body">
+          <p className="card-text">{resObj.description}</p>
+        </div>
+        <div className="card-body">Here we will list reviews</div>
+        <div className="card-body">
+          <a href="">Link for Edit Restaurant</a>
+
+          <a href="">Link for delete Restaurant</a>
+        </div>
+      </div>
+    );
+  };
+  render() {
+    return <div>{this.renderRestaurant()}</div>;
+  }
 }
-
-{/* <div className='card mb-3' style={{width: '75%'}}>
-                <h3 className='card-header'>{this.props.restaurant.name}</h3>
-                <div className='card-body'>
-                    <h6 className='card-subtitle text-muted'>{this.props.restaurant.res_type}</h6>
-                </div>
-                <ImageContainer image={this.props.restaurant.image} />
-                <div className='card-body'>
-                    <p className="card-text">
-                        {this.props.restaurant.description}
-                    </p>
-                </div>
-                <div className='card-body'>
-                    Here we will list reviews
-                </div>
-                <div className='card-body'>
-                    <a href="">Link for Edit Restaurant</a>
-                    
-                    <a href="">Link for delete Restaurant</a>
-                </div>
-
-            </div> */}
