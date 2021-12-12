68e0b3981d6359817952b94aff07b549898c4d0a
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-12-12 01:12:43 -0600

    for each review render 'ReviewCard'

--- src/components/review/Reviews.js
+++ src/components/review/Reviews.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
+import ReviewCard from './ReviewCard'
 
 export default class Reviews extends Component {
+
+    renderReviews = () => {
+        return this.props.reviews.map(review => (
+        <ReviewCard key={review.id} review={review} />
+        ))
+    }
     render() {
         return (
             <div>
-                
+                {this.renderReviews()}
             </div>
         )
     }
