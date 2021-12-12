86deb74625ea4236df5872ef5e850aca445eddaa
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-12-12 01:14:27 -0600

    basic setup for 'ReviewCard'

--- src/components/review/ReviewCard.js
+++ src/components/review/ReviewCard.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react'
 export default class ReviewCard extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="card-header">
+                <p>Mark: {this.props.review.mark}</p>
+                <p>Content: {this.props.review.content}</p>
+
             </div>
         )
     }
