ab42b174764de5dae506905884c4451d0d723233
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-12-13 13:56:15 -0600

    cleanup

--- src/components/ImageContainer.js
+++ src/components/ImageContainer.js
@@ -1,13 +1,7 @@
 import React, { Component } from 'react'
 
 export default class ImageContainer extends Component {
-    // constructor(props){
-    //     super(props)
-
-    // }
     render() {
-    console.log(`${this.props.image}`)
-
         return (
             <div>
                 <img src={this.props.image} alt="restaurant image"  style={{width: '50%', height: '140px'}}/>
