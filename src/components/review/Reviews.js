3080eefc801b3e489b8efce0a274bfbf2b5e8767
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-12-13 11:30:55 -0600

    move logic from render method

--- src/components/review/Reviews.js
+++ src/components/review/Reviews.js
@@ -1,39 +1,50 @@
-import React from "react";
-import ReviewCard from "./ReviewCard";
-import ReviewForm from "./ReviewForm";
+import React from 'react'
+import ReviewCard from './ReviewCard'
+import ReviewForm from './ReviewForm'
 
-class Reviews extends React.Component{
-  constructor(props){
+class Reviews extends React.Component {
+  constructor(props) {
     super(props)
-    this.state ={
+    this.state = {
       addNewReview: false,
-      toggleForm: () => this.toggleForm()
+      toggleForm: () => this.toggleForm(),
     }
   }
 
   toggleForm = () => {
     this.setState((prevState) => ({
-      addNewReview: !prevState
+      addNewReview: !prevState,
     }))
   }
   handleNewReviewClick = () => {
     this.setState({
-      addNewReview: true
+      addNewReview: true,
     })
   }
   renderReviews = () => {
-    return this.props.reviews.map((review) => (
-      <ReviewCard key={review.id} review={review} />
-    ));
-  };
-  render(){
-    return (
-    <div>
-      {this.state.addNewReview ? <ReviewForm restaurant={this.props.restaurant} toggleForm={this.state.toggleForm} /> : <button onClick={this.handleNewReviewClick}>Add new Review</button>}
-      {this.props.reviews.length === 0 ? <p>No reviews for this restaurant</p> : this.renderReviews()}
-    </div>
-    );
+    return this.props.reviews.length === 0 ? (
+      <p>No reviews for this restaurant</p>
+    ) : (
+      this.props.reviews.map((review) => (
+        <ReviewCard key={review.id} review={review} />
+      ))
+    )
   }
-};
+  render() {
+    return (
+      <div>
+        {this.state.addNewReview ? (
+          <ReviewForm
+            restaurant={this.props.restaurant}
+            toggleForm={this.state.toggleForm}
+          />
+        ) : (
+          <button onClick={this.handleNewReviewClick}>Add new Review</button>
+        )}
+        {this.renderReviews()}
+      </div>
+    )
+  }
+}
 
-export default Reviews;
+export default Reviews
