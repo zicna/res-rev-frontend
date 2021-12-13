d79c3ab9af39bc4152ef3974181ae52ae1217087
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-12-13 13:56:51 -0600

    remove bootswatch import

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import App from './App';
 import { Provider } from 'react-redux'
 import store from './redux/store'
 
-import 'bootswatch/dist/materia/bootstrap.min.css'
-
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
