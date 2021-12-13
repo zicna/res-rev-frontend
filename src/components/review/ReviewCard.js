0f0eb40eb7e2e36eb731c0714ecf7ea03d5d3057
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-12-13 14:11:13 -0600

    fix typo

--- src/components/review/ReviewCard.js
+++ src/components/review/ReviewCard.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 
 export default class ReviewCard extends Component {
-    render() {
-        return (
-            <div className="card">
-                <div classnName='num-display'>{this.props.review.mark}</div>
-                <div className='text-display'>Content: {this.props.review.content}</div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="card">
+        <div className="num-display">{this.props.review.mark}</div>
+        <div className="text-display">Content: {this.props.review.content}</div>
+      </div>
+    )
+  }
 }
