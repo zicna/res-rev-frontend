6c2bfc39bf0cc618d8e04ab74d93080d984c9feb
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-12-13 13:45:51 -0600

    add index.css file and import it to index.js file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css'
 import App from './App';
 
 import { Provider } from 'react-redux'
@@ -10,7 +11,9 @@ import 'bootswatch/dist/materia/bootstrap.min.css'
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App className="list-group"/>
+      <div className="container">
+        <App />
+      </div>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
