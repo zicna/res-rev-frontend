3047ecde7bcbfb060e51a3d246293ed7cda2872f
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-12-13 16:46:42 -0600

    reformat ReviewForm and add styling

--- src/components/review/ReviewForm.js
+++ src/components/review/ReviewForm.js
@@ -1,56 +1,49 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { addReview } from '../../redux/actions/reviewAction'
 
 class ReviewForm extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      content: "",
-      mark: "",
+      content: '',
+      mark: '',
       restaurnt_id: this.props.restaurant.id,
-    };
+    }
   }
 
   handleChange = (event) => {
     this.setState({
-        [event.target.name]: event.target.value
+      [event.target.name]: event.target.value,
     })
   }
   handleSubmit = (event) => {
     event.preventDefault()
-    const {content, mark, restaurant_id} = this.state
-    const reviewObj ={
-        review:{
-            content,
-            mark,
-            restaurant_id
-        }
+    const { content, mark, restaurant_id } = this.state
+    const reviewObj = {
+      review: {
+        content,
+        mark,
+        restaurant_id,
+      },
     }
     // * call method form props, after connection to Redux
     this.props.dispatchAddReview(reviewObj)
 
     // * reset local state
     this.setState({
-        content: '',
-        mark: '',
-        restaurant_id: this.props.restaurant.id
-
+      content: '',
+      mark: '',
+      restaurant_id: this.props.restaurant.id,
     })
-  }
-  handleCancel = () => {
-    this.props.toggleForm()
   }
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <fieldset>
-          <legend>
-            New Review for {this.props.restaurant.name} restaurant
-          </legend>
-          <div>
-            <label>Your review:</label>
-            <div>
+      <div className="card">
+        <form onSubmit={this.handleSubmit}>
+          <fieldset>
+            <h2>How would you rate {this.props.restaurant.name} restaurant?</h2>
+            <div className="input-group">
               <input
                 type="textarea"
                 rows="4"
@@ -60,39 +53,33 @@ class ReviewForm extends Component {
                 onChange={this.handleChange}
                 required
                 autoFocus
+                placeholder="Write a review"
               />
+              <label>Mark:</label>
+              <div>
+                <input
+                  type="number"
+                  min={1}
+                  max={10}
+                  name="mark"
+                  value={this.state.mark}
+                  onChange={this.handleChange}
+                  required
+                />
+              </div>
+              <input type="submit" value="Send" />
             </div>
-          </div>
-          <div>
-            <label>Mark:</label>
-            <div>
-              <input
-                type="number"
-                min={1}
-                max={10}
-                name="mark"
-                value={this.state.mark}
-                onChange={this.handleChange}
-                required
-              />
-            </div>
-          </div>
-          <div>
-            <button onClick={this.handleCancel}>Cancel</button>
-          </div>
-          <div>
-            <input type="submit" value="Create New Review" />
-          </div>
-        </fieldset>
-      </form>
-    );
+          </fieldset>
+        </form>
+      </div>
+    )
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        dispatchAddReview: (reviewObject) => dispatch(addReview(reviewObject))
-    }
+  return {
+    dispatchAddReview: (reviewObject) => dispatch(addReview(reviewObject)),
+  }
 }
 
-export default connect()(ReviewForm)
+export default connect(null, mapDispatchToProps)(ReviewForm)
