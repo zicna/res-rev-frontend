a535587294cb97ee93efc8b8d9c6fe01a4d1641c
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-12-14 22:47:02 -0600

    fix reducer, on both delete action we were mutating state and es should return new state object

--- src/redux/reducers/restaurantReducer.js
+++ src/redux/reducers/restaurantReducer.js
@@ -6,40 +6,34 @@ const restaurantReducer = (state = defState, action) => {
   switch (action.type) {
     case 'GET_RESTAURANTS':
       return {
-        ...state,
         restaurants: action.payload,
       }
     case 'ADD_RESTAURANT':
       return {
-        ...state,
         restaurants: [...state.restaurants, action.payload],
       }
     case 'DELETE_RESTAURANT':
-      const newRestaurants = state.restaurants.filter(
+      const newRestaurants = [...state.restaurants.filter(
         (res) => res.id !== action.payload
-      )
+      )]
       return {
-        ...state,
         restaurants: newRestaurants,
       }
     case 'ADD_REVIEW':
       const restaurantIndex = state.restaurants.findIndex((res) => res.id === action.payload.restaurant_id)
       return {
-        ...state,
         restaurants: [
           ...state.restaurants,
-          state.restaurants[restaurantIndex].reviews.push(action.payload),
+          state.restaurants[restaurantIndex].reviews.unshift(action.payload),
         ],
       }
       case 'DELETE_REVIEW':
-        // debugger
         const resIndex = state.restaurants.findIndex((res) => res.id === action.payload.restaurant_id)
-
+        let resReviews = [...state.restaurants[resIndex].reviews.filter(review => review.id !== action.payload.id)]
         return{
-          ...state,
           restaurants: [
             ...state.restaurants,
-            state.restaurants[resIndex].reviews.filter(review => review.id !== action.payload.id)
+            state.restaurants[resIndex].reviews = [...resReviews]
           ]
         }
     default:
