8ed017578385c4776b27b85d24fe2271e550d08e
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-12-16 15:31:19 -0600

    add links for contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import { FaBlog, FaGithub, FaLinkedin } from 'react-icons/fa'
+import { MdEmail } from 'react-icons/md'
+// import { Link } from 'react-router-dom'
 
 export default function Contact() {
-    return (
-        <div>
-            Hello from Contact Page
-        </div>
-    )
+  return (
+    <div className="card">
+      <div>
+        <a target="_blank" href="https://dev.to/zicna">
+          <FaBlog style={{ color: '#545779', fontSize: '3.5em' }} />
+          Visit my blog
+        </a>
+      </div>
+      <div>
+        <a target="_blank" href="https://github.com/zicna">
+          <FaGithub style={{ color: '#545779', fontSize: '3.5em' }} />
+          Visit my GitHub page
+        </a>
+      </div>
+      <div>
+        <a target="_blank" href="https://www.linkedin.com/feed/">
+          <FaLinkedin style={{ color: '#545779', fontSize: '3.5em' }} />
+          Contact me through Linkedin
+        </a>
+      </div>
+      <div>
+        <a target="_balnk" href="mailto:zivkovicmilan1987@gmail.com">
+          <MdEmail style={{ color: '#545779', fontSize: '3.5em' }} />
+          Email me directly
+        </a>
+      </div>
+    </div>
+  )
 }
