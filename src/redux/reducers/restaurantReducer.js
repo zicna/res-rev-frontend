d6a3c0e3573a0914f8c76e2490979c87118fc3b6
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-12-16 13:30:06 -0600

    add editReview case and declare 'restaurantIndex' outside 'switch block'

--- src/redux/reducers/restaurantReducer.js
+++ src/redux/reducers/restaurantReducer.js
@@ -3,7 +3,10 @@ const defState = {
 }
 
 const restaurantReducer = (state = defState, action) => {
+  let restaurantIndex;
+
   switch (action.type) {
+
     case 'GET_RESTAURANTS':
       return {
         restaurants: action.payload,
@@ -13,30 +16,45 @@ const restaurantReducer = (state = defState, action) => {
         restaurants: [...state.restaurants, action.payload],
       }
     case 'DELETE_RESTAURANT':
-      const newRestaurants = [...state.restaurants.filter(
-        (res) => res.id !== action.payload
-      )]
+      const newRestaurants = [
+        ...state.restaurants.filter((res) => res.id !== action.payload),
+      ]
       return {
         restaurants: newRestaurants,
       }
     case 'ADD_REVIEW':
-      const restaurantIndex = state.restaurants.findIndex((res) => res.id === action.payload.id)
+      restaurantIndex = state.restaurants.findIndex(
+        (res) => res.id === action.payload.id
+      )
       return {
         restaurants: [
           ...state.restaurants.slice(0, restaurantIndex),
-            action.payload,
-            ...state.restaurants.slice(restaurantIndex + 1)
+          action.payload,
+          ...state.restaurants.slice(restaurantIndex + 1),
         ],
       }
-      case 'DELETE_REVIEW':
-        const resIndex = state.restaurants.findIndex((res) => res.id === action.payload.id)
-        return{
-          restaurants: [
-            ...state.restaurants.slice(0, resIndex),
-            action.payload,
-            ...state.restaurants.slice(resIndex + 1)
-          ]
-        }
+    case 'DELETE_REVIEW':
+      restaurantIndex = state.restaurants.findIndex(
+        (res) => res.id === action.payload.id
+      )
+      return {
+        restaurants: [
+          ...state.restaurants.slice(0, restaurantIndex),
+          action.payload,
+          ...state.restaurants.slice(restaurantIndex + 1),
+        ],
+      }
+    case 'EDIT_REVIEW':
+      restaurantIndex = state.restaurants.findIndex(
+        (res) => res.id === action.payload.id
+      )
+      return {
+        restaurants: [
+          ...state.restaurants.slice(0, restaurantIndex),
+          action.payload,
+          ...state.restaurants.slice(restaurantIndex + 1),
+        ],
+      }
     default:
       return state
   }
