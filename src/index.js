2b598447418b9d75e8c551440218c2e3fc318946
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-12-16 09:41:24 -0600

    remove wrapping div around App component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App';
+import App from './App'
 
 import { Provider } from 'react-redux'
 import store from './redux/store'
@@ -9,10 +9,8 @@ import store from './redux/store'
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <div className="container">
-        <App />
-      </div>
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
