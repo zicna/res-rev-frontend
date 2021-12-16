764932a3dcd6a2870958de381d6a237b15af5348
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-12-16 12:49:48 -0600

    style NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,38 +1,29 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React from 'react'
+import { NavLink } from 'react-router-dom'
 import { FaHome } from 'react-icons/fa'
 
-
-
-
-
-export default function MainHeader() {
+export default function NavBar() {
   return (
-      <nav >
-        <div>
-          <ul>
-            <li>
-              <NavLink to="/" >
-                <FaHome /> HOME
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/about" >
-                ABOUT
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/contact" >
-                 CONTACT
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/restaurants" >
-                RESTAURANTS
-              </NavLink>
-            </li>
-          </ul>
-        </div>
+    <>
+      <div className="logo">LOGO</div>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/">
+              <FaHome /> HOME
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">ABOUT</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">CONTACT</NavLink>
+          </li>
+          <li>
+            <NavLink to="/restaurants">RESTAURANTS</NavLink>
+          </li>
+        </ul>
       </nav>
-  );
+    </>
+  )
 }
