59f44866ab6b0e7ae3fbfb87eaccd889ef0013bb
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-12-16 09:40:17 -0600

    rearange component rendering

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import NavBar from "./components/NavBar";
+import NavBar from './components/NavBar'
 import Header from './pages/Header'
 import Footer from './pages/Footer'
 
-import { BrowserRouter as Router } from "react-router-dom";
-import MainContainer from "./containers/MainContainer";
+import { BrowserRouter as Router } from 'react-router-dom'
+import MainContainer from './containers/MainContainer'
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <Router>
-        <NavBar />
+        <Header />
         <MainContainer />
+        <Footer />
       </Router>
-      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
