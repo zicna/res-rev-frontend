974141bae3d455a1e3b770f6d648d98f25c74ee5
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-12-18 15:00:21 -0600

    conditionaly render ReviewForm or ReviewEditForm based of local state

--- src/containers/ReviewContainer.js
+++ src/containers/ReviewContainer.js
@@ -2,12 +2,38 @@ import React from 'react'
 import ReviewForm from '../components/review/ReviewForm'
 import ReviewStats from '../components/review/ReviewStats'
 import Reviews from '../components/review/Reviews'
+import ReviewEditForm from '../components/review/ReviewEditForm'
 
 class ReviewContainer extends React.Component {
+  constructor(props){
+    super(props)
+    this.state ={
+      review: {
+        id:'',
+        restaurant_id:'',
+        content: '',
+        mark: ''
+      },
+      edit:false
+    }
+  }
+
+  handleEditReview = ({id, mark, content, restaurant_id}) => {
+    console.log(`id: ${id}; mark: ${mark}; content: ${content}`)
+    this.setState({
+      review:{
+        id,
+        restaurant_id,
+        mark,
+        content
+      },
+      edit: true
+    })
+  }
   render() {
     return (
       <div>
-        <ReviewForm restaurant={this.props.restaurant} />
+        { this.state.edit ? < ReviewEditForm restaurant={this.props.restaurant} review={this.state.review}/> : <ReviewForm restaurant={this.props.restaurant} />}
         <ReviewStats
           rating={this.props.restaurant.rating}
           review_count={this.props.restaurant.review_count}
@@ -15,6 +41,7 @@ class ReviewContainer extends React.Component {
         <Reviews
           restaurant={this.props.restaurant}
           reviews={this.props.restaurant.reviews}
+          editReview={this.handleEditReview}
         />
       </div>
     )
